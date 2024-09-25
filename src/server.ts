5b8b607ccd3cb926b82c34cf344d4bfbd6b9bf2f
Josué Feitosa <josuehsfeitosa@gmail.com>
2024-09-24 23:08:30 -0300

    instalando express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log("hello world!")
+import express, {json, Request, Response} from "express";
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(json())
+
+app.get("/", (req:Request,res:Response)=>{
+    res.status(200).json({player: "beckham"})
+})
+
+app.listen(port, ()=>{
+    console.log(`🔥 Server running at port http://localhost:${port}`);
+})
